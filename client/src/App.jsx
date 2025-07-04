6134f85231b7cf652ef6936187b73fee98ec94d4
SadiqRaja1 <sortmode314@gmail.com>
2025-07-04 21:14:25 +0530

    tailwindcss added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css"
 const App = () => {
   return (
     <div>
-      <img src={mainLogo} alt="mainImg" />
+      <img src={mainLogo} alt="mainImg" className='bg-black'/>
     </div>
   )
 }
