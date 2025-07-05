69e41032994f58ed81068084e8e7cba4bcbbb022
SadiqRaja1 <sortmode314@gmail.com>
2025-07-05 21:30:54 +0530

    Singup Frontend created

--- client/src/layouts/AuthLayout.jsx
+++ client/src/layouts/AuthLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import "./AuthLayout.css"
 
 const AuthLayout = () => {
   return (
-    <>
+    <div id='authMainDiv' className='min-h-screen w-full flex flex-col bg-blue-700 items-center justify-center'>
         <Outlet />
-    </>
+    </div>
   )
 }
 
