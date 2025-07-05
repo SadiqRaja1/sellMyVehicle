892f925f30383e42edfc936f1ff697901452f2f7
SadiqRaja1 <sortmode314@gmail.com>
2025-07-05 06:10:10 +0530

    added few files like header and footer to work, browserrouter and rotes are created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import mainLogo from "./assets/mainLogo.png"
 import "./App.css"
+import {Route, Routes } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Home from "./pages/Home"
 
 const App = () => {
   return (
-    <div>
-      <img src={mainLogo} alt="mainImg" className='bg-black'/>
-    </div>
+    <>
+      <Header />
+
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+
+      <Footer />
+    </>
+
   )
 }
 
