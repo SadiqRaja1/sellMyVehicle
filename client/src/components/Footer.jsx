d0b89af31dae11b97f1fa02c8d321ba0eff88acf
SadiqRaja1 <sortmode314@gmail.com>
2025-07-05 12:26:59 +0530

    footer content added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-      <footer className='h-10 bg-slate-700 text-gray-200'>
-
+      <footer className='h-10 bg-slate-700 text-gray-200 flex justify-center items-center'>
+        <p>Contact us on - Email: contact@sellMyVehicle.com</p>
       </footer>
     </>
   )
