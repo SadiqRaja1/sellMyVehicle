7d9621630d431d8ee2ac435af7076a9d3defdf84
SadiqRaja1 <sortmode314@gmail.com>
2025-07-07 20:05:16 +0530

    dbConfig and userSchema created

--- server/src/configuration/dbConfig.js
+++ server/src/configuration/dbConfig.js
@@ -1,2 +1,15 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
+async function main() {
+    await mongoose.connect(process.env.Mongo_URL);
+}
+
+main().then( () => {
+    console.log("connected to db")
+}
+).catch((error) => {
+    console.log("Some error in connecting to db", error);
+})
+
+module.exports = mongoose;
