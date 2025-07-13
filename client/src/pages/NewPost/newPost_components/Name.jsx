68e3854c4773a87e8a0d48c3dfc0d5de5c58a502
SadiqRaja1 <sortmode314@gmail.com>
2025-07-13 10:00:50 +0530

    In new post form name field edited and added to select car and bike

--- client/src/pages/NewPost/newPost_components/Name.jsx
+++ client/src/pages/NewPost/newPost_components/Name.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 
 const VehiName = () => {
   return (
-    <div className='col-span-4 grid grid-cols-3 gap-4' >
+    <div className='col-span-4 grid grid-cols-7 gap-4' >
         <div>
+            <label htmlFor="vehicleType">Vehicle type</label>
+            <select className='w-full focus:outline-0 border border-gray-400 rounded px-1 py-1.5' name="vehicleType" id="vehicleType" defaultValue="">
+                <option value="" disabled>--Select--</option>
+                <option value="bike">Bike</option>
+                <option value="car">Car</option>
+            </select>
+        </div>
+        <div className='col-span-2'>
             <label className='block' htmlFor="make">Make</label>
             <input className='w-full focus:outline-0 border border-gray-400 rounded px-1 py-1.5' type="text" name='make' id='make' />
         </div>
-        <div>
+        <div className='col-span-2'>
             <label className='block' htmlFor="model">Model</label>
             <input className='w-full focus:outline-0 border border-gray-400 rounded px-1 py-1.5' type="text" name='model' id='model' />              
         </div>
-        <div>
+        <div className='col-span-2'>
             <label className='block' htmlFor="variant">Variant</label>
             <input className='w-full focus:outline-0 border border-gray-400 rounded px-1 py-1.5' type="text" name='variant' id='variant' />
         </div>
