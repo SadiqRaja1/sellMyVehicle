5c710fe15bb71ddc156cbfe22263f9fa78fa757c
SadiqRaja1 <sortmode314@gmail.com>
2025-07-21 06:46:30 +0530

    NewPost changed to simple

--- server/src/models/vehicle/vehicle.js
+++ server/src/models/vehicle/vehicle.js
@@ -39,7 +39,7 @@ const vehicleSchema = new mongoose.Schema({
         required: true
     },
 
-    State:{
+    state:{
         type:String,
         enum: ["Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chhattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal"],
         required: true,
@@ -82,13 +82,13 @@ const vehicleSchema = new mongoose.Schema({
         required:true
     },
 
-    show:{
+    insurance_show:{
         type:String,
         required: true,
         enum : ["Zero Depreciation", "First-Party", "Third-Party Insurance", "Expired"]
     },
 
-    valid_Till : {
+    insurance_valid_Till : {
         type:Date,
         required: true
     },
